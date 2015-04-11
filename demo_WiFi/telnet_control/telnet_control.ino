45ec3132846743706d6af0b0649dc0c3a2edd38d
Robert Spitzenpfeil <robert@linuxbox.site>
2015-04-11 21:23:49 +0200

    Adjusted WiFi code to specific "AT-firmware".

--- demo_WiFi/telnet_control/telnet_control.ino
+++ demo_WiFi/telnet_control/telnet_control.ino
@@ -1,9 +1,9 @@
 #define ESP_SSID (F("test-ssid"))
 #define ESP_PASS (F("test-pass"))
-#define ESP_STATIC_IP (F("192.168.1.8"))
+#define ESP_STATIC_IP (F("192.168.1.15"))
 #define ESP_LINE_TERM (F("\r\n"))
-#define ESP_INIT_CIBAUD 9600UL
-#define ESP_FAST_CIBAUD 115200UL
+#define ESP_INIT_CIOBAUD 9600UL
+#define ESP_FAST_CIOBAUD 115200UL
 
 void setup(void) {
 	increase_ESP8266_baud_rate(); // this may only be needed once or not at all
@@ -24,7 +24,7 @@ void setup(void) {
         delay(2000);
         
 	// set static IP address
-        Serial.print(F("AT+CISTAP=\""));
+        Serial.print(F("AT+CIPSTA=\""));
         Serial.print(ESP_STATIC_IP);
         Serial.print(F("\""));
         Serial.print(ESP_LINE_TERM);
@@ -37,9 +37,9 @@ void loop(void) {
 }
 
 void increase_ESP8266_baud_rate(void) {
-        Serial.begin(ESP_INIT_CIBAUD); // start with low speed
-        Serial.print(F("AT+CIBAUD=\""));
-        Serial.print(ESP_FAST_CIBAUD);
+        Serial.begin(ESP_INIT_CIOBAUD); // start with low speed
+        Serial.print(F("AT+CIOBAUD=\""));
+        Serial.print(ESP_FAST_CIOBAUD);
         Serial.print(F("\""));
         Serial.print(ESP_LINE_TERM);
         delay(50); // wait a bit so stuff has been sent out
