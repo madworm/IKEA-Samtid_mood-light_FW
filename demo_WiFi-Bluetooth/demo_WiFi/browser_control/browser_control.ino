87a690c1c8e6f7efb03b9adb2cdfae28deeb8c95
Robert Spitzenpfeil <robert@linuxbox.site>
2015-06-18 15:44:07 +0200

    HTTP-request parameter extraction works.

--- demo_WiFi-Bluetooth/demo_WiFi/browser_control/browser_control.ino
+++ demo_WiFi-Bluetooth/demo_WiFi/browser_control/browser_control.ino
@@ -58,6 +58,14 @@ void parseCommand(String com_str)
 
 	int index_of_semicolon = com_str.indexOf(":");
 
+	// set default values
+	unsigned int led_mode = 0;
+	unsigned int led_start = 0;
+	unsigned int led_stop = (LEDS - 1);
+	unsigned int led_hue = 0;
+	unsigned int led_sat = 255;
+	unsigned int led_val = 255;
+
 	if (index_of_semicolon != -1) {
 		// found the ":" in "+IPD,0,297:GET..."
 		request_ctr++;
@@ -74,7 +82,7 @@ void parseCommand(String com_str)
 		Serial.print(F("AT+CIPSEND="));
 		Serial.print(incoming_connection_number);
 		Serial.print(F(","));
-		Serial.print(11 + maincmd_str.length() + 1 + 16 + number_of_digits(maincmd_str.length()) + 1 + 15 + number_of_digits(request_ctr) + 1);
+		Serial.print(11 + maincmd_str.length() + 1 + 16 + NoD(maincmd_str.length()) + 1 + 15 + NoD(request_ctr) + 1);
 		Serial.print(ESP_LINE_TERM);
 		if (wait_for("OK") != true) {
 			init_ESP8266();
@@ -93,9 +101,6 @@ void parseCommand(String com_str)
 			init_ESP8266();
 			return;
 		}
-		// parsing maincmd_str goes here
-		// proposed format: mode=<...>&start=<...>&stop=<...>&hue=<...>&sat=<...>&val=<...>&
-		// default values: mode = 0, start = 0, stop = (LEDS-1), (hue,sat,val) = current values
 
 		if (maincmd_str.length() <= 14) {
 
@@ -113,7 +118,100 @@ void parseCommand(String com_str)
 				init_ESP8266();
 				return;
 			}
+		} else {
+			// parsing maincmd_str goes here
+			// proposed format: mode=<...>&start=<...>&stop=<...>&hue=<...>&sat=<...>&val=<...>&
+			// default values: mode = 0, start = 0, stop = (LEDS-1), (hue,sat,val) = current values
+
+			// look for mode=<...> etc.
+
+			int index_of_mode = maincmd_str.indexOf("mode=");	// -1 if nothing found
+			if (index_of_mode != -1) {
+				int index_of_mode_term = maincmd_str.indexOf("&", index_of_mode);	// search '&' after 'mode'
+				if (index_of_mode_term != -1) {
+					String tmp_str = maincmd_str.substring(index_of_mode + 5, index_of_mode_term);	// "mode=123&..." take "123"
+					led_mode = (unsigned int)(tmp_str.toInt());
+				}
+			}
+
+			int index_of_start = maincmd_str.indexOf("start=");
+			if (index_of_start != -1) {
+				int index_of_start_term = maincmd_str.indexOf("&", index_of_start);
+				if (index_of_start_term != -1) {
+					String tmp_str = maincmd_str.substring(index_of_start + 6, index_of_start_term);	// "start=234&..." take "234"
+					led_start = (unsigned int)(tmp_str.toInt());
+				}
+			}
+
+			int index_of_stop = maincmd_str.indexOf("stop=");
+			if (index_of_stop != -1) {
+				int index_of_stop_term = maincmd_str.indexOf("&", index_of_stop);
+				if (index_of_stop_term != -1) {
+					String tmp_str = maincmd_str.substring(index_of_stop + 5, index_of_stop_term);	// "stop=456&..." take "456"
+					led_stop = (unsigned int)(tmp_str.toInt());
+				}
+			}
+
+			int index_of_hue = maincmd_str.indexOf("hue=");
+			if (index_of_hue != -1) {
+				int index_of_hue_term = maincmd_str.indexOf("&", index_of_hue);
+				if (index_of_hue_term != -1) {
+					String tmp_str = maincmd_str.substring(index_of_hue + 4, index_of_hue_term);	// "hue=123&..." take "123"
+					led_hue = (unsigned int)(tmp_str.toInt());
+				}
+			}
+
+			int index_of_sat = maincmd_str.indexOf("sat=");
+			if (index_of_sat != -1) {
+				int index_of_sat_term = maincmd_str.indexOf("&", index_of_sat);
+				if (index_of_sat_term != -1) {
+					String tmp_str = maincmd_str.substring(index_of_sat + 4, index_of_sat_term);	// "sat=456&..." take "456"
+					led_sat = (unsigned int)(tmp_str.toInt());
+				}
+			}
+
+			int index_of_val = maincmd_str.indexOf("val=");
+			if (index_of_val != -1) {
+				int index_of_val_term = maincmd_str.indexOf("&", index_of_val);
+				if (index_of_val_term != -1) {
+					String tmp_str = maincmd_str.substring(index_of_val + 4, index_of_val_term);	// "val=789&..." take "789"
+					led_val = (unsigned int)(tmp_str.toInt());
+				}
+			}
+
+			Serial.print(F("AT+CIPSEND="));
+			Serial.print(incoming_connection_number);
+			Serial.print(F(","));
+			Serial.print(NoD(led_mode) + NoD(led_start) + NoD(led_stop) + NoD(led_hue) + NoD(led_sat) + NoD(led_val) + 40);
+			Serial.print(ESP_LINE_TERM);
+			if (wait_for("OK") != true) {
+				init_ESP8266();
+				return;
+			}
+			Serial.print(F("mode: "));	// 6
+			Serial.print(led_mode);
+			Serial.print(F("\n"));	// 1                        
+			Serial.print(F("start: "));	// 7
+			Serial.print(led_start);
+			Serial.print(F("\n"));	// 1                                                
+			Serial.print(F("stop: "));	// 6
+			Serial.print(led_stop);
+			Serial.print(F("\n"));	// 1                                                
+			Serial.print(F("hue: "));	// 5
+			Serial.print(led_hue);
+			Serial.print(F("\n"));	// 1                                                
+			Serial.print(F("sat: "));	// 5
+			Serial.print(led_sat);
+			Serial.print(F("\n"));	// 1                                                
+			Serial.print(F("val: "));	// 5
+			Serial.print(led_val);
+			Serial.print(F("\n"));	// 1                                                
+			if (wait_for("OK") != true) {
+				init_ESP8266();
+				return;
+			}
 		}
+
 		// bye bye
 		Serial.print(F("AT+CIPSEND="));
 		Serial.print(incoming_connection_number);
@@ -231,7 +329,7 @@ void toggle_2nd_WS(void)
 
 }
 
-unsigned int number_of_digits(unsigned int number)
+unsigned int NoD(unsigned int number)
 {
 	unsigned int digits = 0;
 	unsigned int temp;
